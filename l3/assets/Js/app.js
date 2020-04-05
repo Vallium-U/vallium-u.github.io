4d9d5b06939f7840dcf8b6ffa43598e17c90a480
Max <mlobaruov@gmail.com>
2020-04-05 23:08:36 +0300

    statistics

--- l3/assets/Js/app.js
+++ l3/assets/Js/app.js
@@ -58,11 +58,39 @@ $(function () {
         slidesToShow: 1,
         slidesToScroll: 1
 
-
-
-
     });
 
 
+
+
+    // statistic
+    var time = 5, cc = 1;
+    $(window).scroll(function () {
+        $('#statistics').each(function () {
+            var cPos = $(this).offset().top,
+                topWindow = $(window).scrollTop();
+            if (cPos < topWindow + 400) {
+
+                if (cc < 2) {
+                    $('.stat__count').addClass('stat__count__visibility');
+                    $("div").each(function () {
+                        var i = 1,
+                            num = $(this).data('num'),
+                            step = 1000 * time / num,
+                            that = $(this),
+                            int = setInterval(function () {
+                                if (i <= num) {
+                                    that.html(i);
+                                } else {
+                                    cc = cc + 2;
+                                    clearInterval(int);
+                                }
+                                i++;
+                            }, step);
+                    });
+                }
+            }
+        });
+    });
 
 });
