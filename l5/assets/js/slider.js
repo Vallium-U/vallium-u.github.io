2bab84137e9c6011f861514afea91ace3d77e355
Max <43815952+Vallium-U@users.noreply.github.com>
2020-05-01 20:05:27 +0300

    1

---
+++ l5/assets/js/slider.js
@@ -0,0 +1,11 @@
+$(function() {
+    // $("[data-slider]").slick({
+    //     arrows: false,
+    //     dots: true,
+    //     infinite: true,
+    //     speed: 1500,
+    //     //fade: true,
+    //     slidesToShow: 1,
+    //     slidesToScroll: 1
+    // });
+});
