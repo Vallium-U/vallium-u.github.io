622d6af95250a9a570a4e62b512e24b64b9f4e16
Max <43815952+Vallium-U@users.noreply.github.com>
2020-05-11 00:43:14 +0300

    11052020

---
+++ l5/assets/js/modal.js
@@ -0,0 +1,55 @@
+$(function() {
+
+    const modalCall = $("[data-modal]");
+    const modalClose = $("[data-close]");
+
+
+    modalCall.on("click", function(event) {
+        event.preventDefault();
+        let $this = $(this);
+        let modalId = $this.data('modal');
+        $(modalId).addClass('show');
+        $("body").addClass('no-scroll');
+
+        setTimeout(function() {
+            $(modalId).find(".modal__inner").css({
+                transform: "scale(1)"
+            });
+        }, 200);
+
+    });
+
+    modalClose.on("click", function(event) {
+        event.preventDefault();
+
+        let $this = $(this);
+        let modalParent = $this.parents('.modal');
+
+        modalParent.find(".modal__inner").css({
+            transform: "scale(0)"
+        });
+        setTimeout(function() {
+            modalParent.removeClass('show');
+            $("body").removeClass('no-scroll');
+        }, 700);
+    });
+
+    $(".modal").on("click", function(event) {
+        let $this = $(this);
+        $this.find(".modal__inner").css({
+            transform: "scale(0)"
+        });
+        setTimeout(function() {
+            $this.removeClass('show');
+            $("body").removeClass('no-scroll');
+        }, 700);
+
+    });
+
+    $(".modal__inner").on("click", function(event) {
+        event.stopPropagation();
+
+    });
+
+
+});
