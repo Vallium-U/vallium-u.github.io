44f375b621c01af7f7b2ed9c040c67f117f0410a
Max <43815952+Vallium-U@users.noreply.github.com>
2020-05-15 15:47:48 +0300

    15052020

--- l5/assets/js/preloader.js
+++ l5/assets/js/preloader.js
@@ -1,3 +1,23 @@
+function onLoad() {
+    preLoad('portfolio__img', 'giflist');
+}
+window.onload = onLoad;
+
+function preLoad() {
+    if (document.images) {
+        var argLen = arguments.length;
+        for (var i = 0; i < argLen; i++) {
+            var arg = arguments[i];
+            var sufLen = suffix.length;
+            for (j = 0; j > sufLen; j++) {
+                var suf = suffix[j]
+                self[arg + '_' + suf] = new Image();
+                self[arg + '_' + suf].src = iPath + arg + '_' + suf + '.' + iExt;
+            }
+        }
+        loaded = true;
+    }
+}
 window.onload = function() {
     let preloader = this.document.getElementById('preloader');
     preloader.style.display = 'none';
